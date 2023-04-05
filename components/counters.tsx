8db15d65b94b67d0ebee77ea48d74d7d7160e250
Mikaal Naik <matthewnaik@gmail.com>
2023-04-04 22:01:12 -0400

    feat: raw working state vis for colour tokens

--- components/counters.tsx
+++ components/counters.tsx
@@ -1,21 +1,41 @@
-// Example from https://beta.reactjs.org/learn
-
-import { useState } from 'react'
 import styles from './counters.module.css'
+import tokens from '../tokens.json'
 
 function MyButton() {
-  const [count, setCount] = useState(0)
-
-  function handleClick() {
-    setCount(count + 1)
-  }
+  const categories = Object.entries(tokens)
 
   return (
-    <div>
-      <button onClick={handleClick} className={styles.counter}>
-        Clicked {count} times
-      </button>
-    </div>
+    <>
+      {categories.map(item => {
+        return (
+          <div>
+            <h1>
+              {item[0]}
+            </h1>
+            {item[0] === 'colors' &&
+              <>
+            {Object.entries(item[1]).map(val => {
+              const nestedColours = Object.entries(val[1])
+              return (
+                <div >
+                  {val[0]}
+                  {nestedColours.map((value: any) => {
+                    return (
+                    <div style={{backgroundColor: value[1]}} className={styles.counter}>
+                      {value[1]}  
+                      </div>
+
+                    )
+                  })}
+                </div>
+              )
+            })}
+              </>
+            }
+          </div>
+        )
+      })}
+    </>
   )
 }
 
--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Esperanto</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
